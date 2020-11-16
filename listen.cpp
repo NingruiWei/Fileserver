0ee41010cf4755a92fb3c35af2a0d0a61ab3f4a3
root <root@Ningruis-PC.localdomain>
2020-11-15 21:49:39 -0500

    genshin impact

--- listen.cpp
+++ listen.cpp
@@ -2,15 +2,31 @@
 #include <iostream>
 #include <unordered_map>
 #include "fileserver.h"
+#include <sys/socket.h>
+#include <unistd.h>
+#include <cstring>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+#include <netdb.h>
+#include <string.h>
+
 using namespace std;
+
 
 int main(int argc, char** argv){
     Fileserver main_fileserver;
     main_fileserver.fill_password_map();
-    int socket();
-    
-    
-    
+    int port = atoi(argv[1]);
+    int sock = socket(AF_INET, SOCK_STREAM, 0);
+    struct sockaddr_in addr;
+
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = htonl(INADDR_ANY);
+    addr.sin_port = htons(port);
+
+    bind(sock, (struct sockaddr*) &addr, sizeof(addr));
+
     
     
     return 0;
